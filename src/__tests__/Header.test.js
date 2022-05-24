04950b9546f6084d5d159a2313d0ee8f4a2575cc
Julien Hnery <jwh_917@yahoo.com>
2022-05-23 22:34:06 -0400

    Completed Assignment

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -7,5 +7,13 @@ import Header from "../components/Header";
 test("displays the text 'hello from the Header!'", () => {
   render(<Header />);
 
-  expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
+  const element = screen.queryByText("hello from the Header!");
+
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
+
+  // screen.debug();
+
+  // expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
 });
